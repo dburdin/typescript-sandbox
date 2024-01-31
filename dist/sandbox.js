8692246c405abe4e624ecb373d1a70401ca85af2
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2024-01-31 11:10:42 +0200

    Task

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -1,6 +1,16 @@
-// let numbers = reverse<number>([1, 2, 3, 4, 5])
-// let strings = reverse<string>(["1, 2, 3, 4, 5"])
-const user = {
-    name: "Lola",
+//! #1 Case
+// const func = (n: number): string[] => {
+//   let answer: string[] = [];
+const thirdFunc = (...numbers) => {
+    let answer = [];
+    const conditions = [
+        { condition: (n) => n % 3 === 0 && n % 5 === 0, output: "FizzBuzz" },
+        { condition: (n) => n % 5 === 0, output: "Buzz" },
+        { condition: (n) => n % 3 === 0, output: "Fizz" },
+    ];
+    return numbers.map((num) => {
+        const matchedCondition = conditions.find((condition) => condition.condition(num));
+        return matchedCondition ? matchedCondition.output : num.toString();
+    });
 };
-console.log(user);
+console.log(thirdFunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,19 +1,78 @@
-// let numbers = reverse<number>([1, 2, 3, 4, 5])
-// let strings = reverse<string>(["1, 2, 3, 4, 5"])
+//! #1 Case
+// const func = (n: number): string[] => {
+//   let answer: string[] = [];
 
-// function reverse<K>(items: K[]): K[] {
-//     return items.reverse()
-// }
+//   for (let i = 1; i <= n; i += 1) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       answer.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       answer.push("Fizz");
+//     } else if (i % 5 === 0) {
+//       answer.push("Buzz");
+//     } else {
+//       answer.push(i.toString());
+//     }
+//   }
 
-// console.log(numbers, strings)
+//   return answer;
+// };
 
-type User = {
-    name: string;
-    age: number;
-}
+// console.log(func(15));
 
-const user: Pick<User, "name"> = {
-    name: "Lola",
-}
+//! #2 Case
+// type Conditions = {
+//   condition: (n: number) => boolean;
+//   output: string;
+// };
 
-console.log(user)
+// const secondFunc = (n: number[]): string[] => {
+//   let answer: string[] = [];
+
+//   let min: number = Math.min(...n);
+//   let max: number = Math.max(...n);
+
+//   const conditionsCollection: Conditions[] = [
+//     { condition: (n) => n % 3 === 0 && n % 5 === 0, output: "FizzBuzz" },
+//     { condition: (n) => n % 5 === 0, output: "Buzz" },
+//     { condition: (n) => n % 3 === 0, output: "Fizz" },
+//   ];
+
+//   for (let i = min; i <= max; i += 1) {
+//     const matchedCondition = conditionsCollection.find((item) => item.condition(i));
+
+//     if (matchedCondition) {
+//       answer.push(matchedCondition.output);
+//     } else {
+//       answer.push(i.toString());
+//     }
+//   }
+
+//   return answer;
+// };
+
+// console.log(secondFunc([1, 15]));
+// console.log(secondFunc([15, 1]));
+
+//! #3 Case
+
+type Condition = {
+  condition: (n: number) => boolean;
+  output: string;
+};
+
+const thirdFunc = (...numbers: number[]): string[] => {
+  let answer: string[] = [];
+
+  const conditions: Condition[] = [
+    { condition: (n) => n % 3 === 0 && n % 5 === 0, output: "FizzBuzz" },
+    { condition: (n) => n % 5 === 0, output: "Buzz" },
+    { condition: (n) => n % 3 === 0, output: "Fizz" },
+  ];
+
+  return numbers.map((num) => {
+    const matchedCondition = conditions.find((condition) => condition.condition(num));
+    return matchedCondition ? matchedCondition.output : num.toString();
+  });
+};
+
+console.log(thirdFunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
